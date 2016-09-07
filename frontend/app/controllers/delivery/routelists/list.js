eaf94b3cd683e0557e82a7bcf219376e511a7391
Konstantin Salikhov <koka58@yandex.ru>
2016-09-07 14:16:23 +0400

    PDF download

--- frontend/app/controllers/delivery/routelists/list.js
+++ frontend/app/controllers/delivery/routelists/list.js
@@ -1,10 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    session: Ember.inject.service(),
+
     actions : {
       downloadPDF(routelist) {
-          //TODO: implement me
-          console.log("Download pdf for routelist " + routelist.get("id"))
+        this.get('session').authorize('authorizer:custom', (header, value) => {
+          const filename = `${encodeURIComponent(routelist.get("id"))}.pdf`;
+          Ember.$.ajax(`/routelists/${filename}`, {
+            headers : {
+              [header] : value
+            },
+            xhrFields : {
+              responseType: 'blob'
+            }
+          }).then( pdf => {
+            const url = window.URL.createObjectURL(pdf);
+            try {
+              const a = document.createElement('a');
+              a.href = url;
+              a.download = filename;
+              a.click();
+            } finally {
+              window.URL.revokeObjectURL(url);
+            }
+          });
+        });
       },
 
       editRoutelist(routelist) {
